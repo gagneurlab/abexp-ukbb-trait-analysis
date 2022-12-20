985c852d53ea29f4294b56507ad28d26f1b1836e
Jonas Lindner <lindnjon@ouga15.cmm.in.tum.de>
2022-12-20 16:57:17 +0100

    AbExp improves PRS analysis

--- scripts/associate/polygenic_risk_score.py.py
+++ scripts/associate/polygenic_risk_score.py.py
@@ -46,6 +46,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # %%
+import lightgbm as lgb
+
+# %%
 # %matplotlib inline
 # %config InlineBackend.figure_format='retina'
 
@@ -114,11 +117,14 @@ except NameError:
         snakefile = snakefile_path,
         rule_name = 'associate__polygenic_risk_score',
         default_wildcards={
-            # "phenotype_col": "triglycerides_f30870_0_0",
+            #"phenotype_col": "triglycerides_f30870_0_0",
             # "phenotype_col": "standing_height_f50_0_0",
             # "phenotype_col": "body_mass_index_bmi_f21001_0_0",
             # "phenotype_col": "systolic_blood_pressure_automated_reading_f4080_0_0",
-            "phenotype_col": "hdl_cholesterol_f30760_0_0",
+            #"phenotype_col": "hdl_cholesterol_f30760_0_0",
+            #"phenotype_col": "ldl_direct_f30780_0_0",
+            "phenotype_col": "LDL_direct",
+            #"phenotype_col": "Asthma",
             # "feature_set": "LOFTEE_pLoF",
             "feature_set": "AbExp_all_tissues",
             # "feature_set": "LOFTEE_pLoF",
@@ -211,7 +217,7 @@ covariates_df = spark.read.parquet(snakemake.input["covariates_pq"])
 
 # %% {"tags": []}
 covariates_df_columns = covariates_df.columns
-covariates_df_columns
+#covariates_df_columns
 
 # %% [markdown] {"tags": []}
 # ## clumping
@@ -315,7 +321,7 @@ pivot_features_df = (
         *[f.col(g)[s].alias(f"{g}_{s}") for g, s in itertools.product(significant_genes, features)]
     )
 )
-pivot_features_df.printSchema()
+#pivot_features_df.printSchema()
 
 # %%
 gene_features = [f"{g}_{s}" for g, s in itertools.product(significant_genes, features)]
@@ -329,8 +335,7 @@ full_features_df = (
     .join(pivot_features_df, on="individual", how="left")
     .fillna(0, subset=pivot_features_df.columns)
 )
-full_features_df.printSchema()
-
+#full_features_df.printSchema()
 
 # %% [markdown]
 # # select variables
@@ -393,6 +398,17 @@ def get_variables_from_formula(formula, lhs=True, rhs=True):
 full_variables = get_variables_from_formula(formatted_full_formula)
 len(full_variables)
 
+# %%
+#Model with AGE+SEX+PC+PRS
+restricted_variables = full_variables[1:34]
+
+# %%
+#Model with AGE+SEX+PC
+basic_variables = restricted_variables[:-1]
+
+# %%
+abexp_variables = full_variables[35:]
+
 # %% [markdown]
 # # write features
 
@@ -410,10 +426,10 @@ prs_features_df = (
 snakemake.output
 
 # %%
-prs_features_df.write.parquet(snakemake.output["prs_features_pq"], mode="overwrite")
+#prs_features_df.write.parquet(snakemake.output["prs_features_pq"], mode="overwrite")
 
 # %%
-prs_features_df = spark.read.parquet(snakemake.output["prs_features_pq"])
+#prs_features_df = spark.read.parquet(snakemake.output["prs_features_pq"])
 
 # %%
 prs_features_pd_df = prs_features_df.toPandas()
@@ -422,9 +438,13 @@ prs_features_pd_df = prs_features_df.toPandas()
 spark.sparkContext._jvm.System.gc()
 
 # %%
-prs_features_pd_df.head()
+#Drop columns with no information
+full_variables = prs_features_pd_df.columns[prs_features_pd_df.nunique() > 1]
+prs_features_pd_df = prs_features_pd_df[full_variables]
+prs_features_pd_df["sex_f31_0_0"] = prs_features_pd_df["sex_f31_0_0"].astype(int)
 
 # %%
+prs_features_pd_df
 
 # %% [markdown]
 # # read samples
@@ -448,11 +468,12 @@ test_samples
 prs_features_pd_df = prs_features_pd_df.merge(samples, on="individual", how="inner")
 prs_features_pd_df
 
+# %%
+prs_features_pd_df = prs_features_pd_df.dropna()
+prs_features_pd_df
+
 # %% [markdown]
 # ## prepare splits
-
-# %%
-non_test_idx
 
 # %% [markdown]
 # # perform regression
@@ -483,38 +504,18 @@ test_df = prs_features_pd_df[prs_features_pd_df["individual"].isin(test_samples)
 test_df
 
 # %%
-X_train = train_df[restricted_variables].drop(columns=phenotype_col)
+X_train = train_df[full_variables].drop(columns=[phenotype_col, "individual"])
 y_train = train_df[phenotype_col]
 
 # %%
-X_test = test_df[restricted_variables].drop(columns=phenotype_col)
+X_test = test_df[full_variables].drop(columns=[phenotype_col,  "individual"])
 y_test = test_df[phenotype_col]
 
 # %% [markdown]
 # ## train models
 
 # %%
-# TODO for Jonas
-
-# %% [raw]
-# ### restricted model
-
-# %% [raw]
-# model_pipeline = sklearn.pipeline.Pipeline([
-#     ("normalize", sklearn.preprocessing.StandardScaler()),
-#     ("linear_model", sklearn.linear_model.ElasticNetCV(cv=cv_split)),
-# ])
-
-# %% [raw]
-# restricted_model = model_pipeline.fit(X_train, y_train)
-# restricted_model
-
-# %% [raw]
-# restricted_pred_test = restricted_model.predict(X_test)
-# restricted_pred_test
-
-# %% [raw]
-# sklearn.metrics.r2_score(y_test, restricted_pred_test)
+# Continuous traits:
 
 # %% [raw]
 # ### full model
@@ -522,7 +523,7 @@ y_test = test_df[phenotype_col]
 # %%
 model_pipeline = sklearn.pipeline.Pipeline([
     ("normalize", sklearn.preprocessing.StandardScaler()),
-    ("linear_model", sklearn.linear_model.ElasticNetCV(cv=cv_split)),
+    ("linear_model", sklearn.linear_model.ElasticNetCV(cv=cv_split, n_jobs=16)),
 ])
 
 # %%
@@ -536,48 +537,108 @@ full_pred_test
 # %%
 sklearn.metrics.r2_score(y_test, full_pred_test)
 
-# %%
 
 # %%
+def calculate_r2(x, y):
+    """Calculate the r2 coefficient."""
+    # Calculate means
+    x_mean = x.mean()
+    y_mean = y.mean()
+    
+    # Calculate corr coeff
+    numerator = np.sum((x - x_mean)*(y - y_mean))
+    denominator = ( np.sum((x - x_mean)**2) * np.sum((y - y_mean)**2) )**.5
+    correlation_coef = numerator / denominator
+    
+    return correlation_coef**2
 
-# %% {"tags": []}
-import statsmodels.formula.api as smf
-
-# %%
-restricted_model = smf.ols(
-    formatted_restricted_formula,
-    data = prs_features_pd_df
-).fit()
-
-# %%
-full_model = smf.ols(
-    formatted_full_formula,
-    data = prs_features_pd_df
-).fit()
 
 # %%
-lr_stat, lr_pval, lr_df_diff = full_model.compare_lr_test(restricted_model)
+calculate_r2(y_test, full_pred_test)
 
 # %%
-lr_pval
+### restricted model (Without AbExp)
 
 # %%
-lr_df_diff
+model_pipeline_res = sklearn.pipeline.Pipeline([
+    ("normalize", sklearn.preprocessing.StandardScaler()),
+    ("linear_model", sklearn.linear_model.ElasticNetCV(cv=cv_split, n_jobs=16)),
+])
 
 # %%
-prs_features_pd_df.dropna().shape
+restricted_model = model_pipeline_res.fit(X_train[restricted_variables], y_train)
+restricted_model
 
 # %%
-prs_features_pd_df.shape
+restricted_pred_test = restricted_model.predict(X_test[restricted_variables])
+restricted_pred_test
 
 # %%
+sklearn.metrics.r2_score(y_test, restricted_pred_test)
+
+# %%
+calculate_r2(y_test, restricted_pred_test)
+
+# %%
+### basic model (Without PRS)
+
+# %%
+model_pipeline_basic = sklearn.pipeline.Pipeline([
+    ("normalize", sklearn.preprocessing.StandardScaler()),
+    ("linear_model", sklearn.linear_model.ElasticNetCV(cv=cv_split, n_jobs=16)),
+])
+
+# %%
+basic_model = model_pipeline_basic.fit(X_train[basic_variables], y_train)
+basic_model
+
+# %%
+basic_pred_test = basic_model.predict(X_test[basic_variables])
+basic_pred_test
+
+# %%
+sklearn.metrics.r2_score(y_test, basic_pred_test)
+
+# %%
+### LGBMR
+
+# %%
+gbm_full = lgb.LGBMRegressor()
+gbm_full.fit(X_train, y_train)
+full_pred_test = gbm_full.predict(X_test)
+sklearn.metrics.r2_score(y_test, full_pred_test)
+
+# %%
+gbm_restricted = lgb.LGBMRegressor()
+gbm_restricted.fit(X_train[restricted_variables], y_train)
+restricted_pred_test = gbm_restricted.predict(X_test[restricted_variables])
+sklearn.metrics.r2_score(y_test, restricted_pred_test)
+
+# %%
+gbm_basic = lgb.LGBMRegressor()
+gbm_basic.fit(X_train[basic_variables], y_train)
+basic_pred_test = gbm_basic.predict(X_test[basic_variables])
+sklearn.metrics.r2_score(y_test, basic_pred_test)
+
+# %%
+lgb.plot_importance(gbm_full, height=.5, max_num_features=40)
+
+# %%
+nr_of_quantiles = 10
+
 pred_df = (
-    prs_features_pd_df[["individual", phenotype_col]]
+    test_df[["individual", phenotype_col]]
     .rename(columns={phenotype_col: "measurement"})
     .assign(**{
         "phenotype_col": phenotype_col,
-        "full_model_pred": full_model.predict(prs_features_pd_df),
-        "restricted_model_pred": restricted_model.predict(prs_features_pd_df),
+        "phenotype_quantile": np.array(pd.qcut(test_df[phenotype_col], nr_of_quantiles, labels=False)),
+        "at_risk": np.array(pd.qcut(test_df[phenotype_col], 10, labels=False)) >= nr_of_quantiles-1,
+        "full_model_pred": full_pred_test,
+        "full_model_pred_quantile": np.array(pd.qcut(full_pred_test, nr_of_quantiles, labels=False)),
+        "restricted_model_pred": restricted_pred_test,
+        "restricted_model_pred_quantile": np.array(pd.qcut(restricted_pred_test, nr_of_quantiles, labels=False)),
+        "basic_model_pred": basic_pred_test,
+        "basic_model_pred_quantile": np.array(pd.qcut(basic_pred_test, nr_of_quantiles, labels=False)),
     })
 )
 pred_df
@@ -586,18 +647,192 @@ pred_df
 pred_df = (
     pred_df
     .assign(**{
-        "residuals": pred_df["measurement"] - pred_df["restricted_model_pred"],
-        "full - restricted": pred_df["full_model_pred"] - pred_df["restricted_model_pred"],
-        
+        "residuals_full": pred_df["measurement"] - pred_df["full_model_pred"],
+        "residuals_restricted": pred_df["measurement"] - pred_df["restricted_model_pred"],
+        "residuals_basic": pred_df["measurement"] - pred_df["basic_model_pred"],
+        "abs_diff_full_restricted_pred": (pred_df["full_model_pred"] - pred_df["basic_model_pred"]).abs(),
+        "full_model_pred_rank": pred_df["full_model_pred"].rank(ascending = False),
+        "restricted_model_pred_rank": pred_df["restricted_model_pred"].rank(ascending = False),
+        "basic_model_pred_rank": pred_df["basic_model_pred"].rank(ascending = False),
     })
 )
 pred_df
 
 # %%
-restricted_model.rsquared
+# Plot for continuous
 
 # %%
-full_model.rsquared
+ax = pd.DataFrame({"sex+age+PC": pred_df.groupby("basic_model_pred_quantile")["phenotype_quantile"].mean(),"+PRS": pred_df.groupby("restricted_model_pred_quantile")["phenotype_quantile"].mean(), "+AbExpAllTissuesForSignificantGenes": pred_df.groupby("full_model_pred_quantile")["phenotype_quantile"].mean()}).plot()
+ax.set_ylabel("Measurement mean decile")
+ax.set_xlabel("Prediction decile")
+
+# %%
+ax = pd.DataFrame({"sex+age+PC": pred_df.groupby("phenotype_quantile").apply(lambda decile: calculate_r2(decile["measurement"],decile["basic_model_pred"])),"+PRS": pred_df.groupby("phenotype_quantile").apply(lambda decile: calculate_r2(decile["measurement"],decile["restricted_model_pred"])),"+AbExpAllTissuesForSignificantGenes": pred_df.groupby("phenotype_quantile").apply(lambda decile: calculate_r2(decile["measurement"],decile["full_model_pred"]))}).plot()
+ax.set_xlabel(f"{phenotype_col} measurement decile")
+ax.set_ylabel("r^2")
+
+# %%
+ax = pd.DataFrame({"sex+age+PC": pred_df.groupby("phenotype_quantile").apply(lambda decile: sklearn.metrics.mean_squared_error(decile["measurement"],decile["basic_model_pred"])),"+PRS": pred_df.groupby("phenotype_quantile").apply(lambda decile: sklearn.metrics.mean_squared_error(decile["measurement"],decile["restricted_model_pred"])),"+AbExpAllTissuesForSignificantGenes": pred_df.groupby("phenotype_quantile").apply(lambda decile: sklearn.metrics.mean_squared_error(decile["measurement"],decile["full_model_pred"]))}).plot()
+ax.set_xlabel(f"{phenotype_col} measurement decile")
+ax.set_ylabel("MSE")
+
+# %%
+ax = pd.DataFrame({"sex+age+PC": pred_df.groupby("phenotype_quantile").apply(lambda decile: (decile["phenotype_quantile"]==decile["basic_model_pred_quantile"]).mean()),"+PRS": pred_df.groupby("phenotype_quantile").apply(lambda decile: (decile["phenotype_quantile"]==decile["restricted_model_pred_quantile"]).mean()),"+AbExpAllTissuesForSignificantGenes": pred_df.groupby("phenotype_quantile").apply(lambda decile: (decile["phenotype_quantile"]==decile["full_model_pred_quantile"]).mean())}).plot()
+ax.set_xlabel("measurement decile")
+ax.set_ylabel("prediction decile match")
+
+# %%
+models = ["full", "restricted", "basic"]
+odd_ratios = {model: {} for model in models}
+for model in models:
+    for q in np.logspace(np.log10(10), np.log10(10000), num=50):
+        top_samples = pred_df.query(f"{model}_model_pred_rank<{q}")
+        bottom_samples = pred_df.query(f"{model}_model_pred_rank>={q}")
+        
+        #Version1:
+        #odd_ratios[model][q] = (top_samples["at_risk"].mean()) / ((bottom_samples["at_risk"]).mean())
+        
+        #Version2:
+        TP = top_samples["at_risk"].sum()
+        TN = (~bottom_samples["at_risk"]).sum()
+        FP = (~top_samples["at_risk"]).sum()
+        FN = bottom_samples["at_risk"].sum()
+        odd_ratios[model][q] = (TP * TN) / (FP * FN)
+        
+        #Both versions are equivalent
+odd_ratios = pd.DataFrame.from_dict(odd_ratios, orient="index").transpose()
+
+# %%
+fig1, ax1 = plt.subplots()
+ax1.plot(odd_ratios["basic"], label="Age+Sex+PC")
+ax1.plot(odd_ratios["restricted"], label="+PRS")
+ax1.plot(odd_ratios["full"], label="+AbExpSignificantGenesAllTissues")
+plt.legend()
+ax1.set_xscale('log')
+ax1.invert_xaxis()
+plt.xlabel("Top n Samples by prediction")
+plt.ylabel("Odds ratio")
+plt.title(f"Models predicting {phenotype_col} - Test for measurement in top decile")
+ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+
+# %%
+model_discrepency_df = pred_df.query("abs_diff_full_restricted_pred>1")
+sklearn.metrics.mean_squared_error(model_discrepency_df["measurement"], model_discrepency_df["full_model_pred"])
+
+# %%
+sklearn.metrics.mean_squared_error(model_discrepency_df["measurement"], model_discrepency_df["restricted_model_pred"])
+
+# %%
+# Binary traits:
+
+# %%
+model_pipeline = sklearn.pipeline.Pipeline([
+    ("normalize", sklearn.preprocessing.StandardScaler()),
+    ("logistic_regression_model", sklearn.linear_model.LogisticRegressionCV(n_jobs=16, cv=cv_split, max_iter=250, verbose=False, penalty = 'elasticnet', solver = 'saga', l1_ratios = [0.5])),
+])
+
+# %%
+full_model = model_pipeline.fit(X_train, y_train)
+full_model
+
+# %%
+full_pred_test = full_model.predict(X_test)
+full_pred_test
+
+# %%
+full_pred_prob_test = full_model.predict_proba(X_test)[::,1]
+full_pred_prob_test
+
+# %%
+full_model.score(X_test, y_test)
+
+# %%
+cm = sklearn.metrics.confusion_matrix(y_test, full_pred_test)
+
+# %%
+cm
+
+# %%
+model_pipeline_res = sklearn.pipeline.Pipeline([
+    ("normalize", sklearn.preprocessing.StandardScaler()),
+    ("logistic_regression_model", sklearn.linear_model.LogisticRegressionCV(n_jobs=16, cv=cv_split, max_iter=250, verbose=False, penalty = 'elasticnet', solver = 'saga', l1_ratios = [0.5])),
+])
+
+# %%
+restricted_model = model_pipeline_res.fit(X_train[restricted_variables], y_train)
+restricted_model
+
+# %%
+restricted_pred_test = restricted_model.predict(X_test[restricted_variables])
+restricted_pred_test
+
+# %%
+restricted_pred_prob_test = restricted_model.predict_proba(X_test[restricted_variables])[::,1]
+restricted_pred_prob_test
+
+# %%
+restricted_model.score(X_test[restricted_variables], y_test)
+
+# %%
+cm = sklearn.metrics.confusion_matrix(y_test, restricted_pred_test)
+
+# %%
+cm
+
+# %%
+model_pipeline_basic = sklearn.pipeline.Pipeline([
+    ("normalize", sklearn.preprocessing.StandardScaler()),
+    ("logistic_regression_model", sklearn.linear_model.LogisticRegressionCV(n_jobs=8, cv=cv_split, class_weight="balanced", max_iter=250, verbose=False, penalty = 'elasticnet', solver = 'saga', l1_ratios = [0.5])),
+])
+
+# %%
+basic_model = model_pipeline_basic.fit(X_train[basic_variables], y_train)
+basic_model
+
+# %%
+basic_pred_test = basic_model.predict(X_test[basic_variables])
+basic_pred_test
+
+# %%
+basic_pred_prob_test = basic_model.predict_proba(X_test[basic_variables])[::,1]
+basic_pred_prob_test
+
+# %%
+basic_model.score(X_test[basic_variables], y_test)
+
+# %%
+sklearn.metrics.confusion_matrix(y_test, basic_pred_test)
+
+# %%
+fpr_full, tpr_full, _ = sklearn.metrics.roc_curve(y_test,  full_pred_prob_test)
+auc_full = sklearn.metrics.roc_auc_score(y_test, full_pred_prob_test)
+
+fpr_res, tpr_res, _ = sklearn.metrics.roc_curve(y_test,  restricted_pred_prob_test)
+auc_res = sklearn.metrics.roc_auc_score(y_test, restricted_pred_prob_test)
+
+fpr_basic, tpr_basic, _ = sklearn.metrics.roc_curve(y_test,  basic_pred_prob_test)
+auc_basic = sklearn.metrics.roc_auc_score(y_test, basic_pred_prob_test)
+
+#create ROC curve
+plt.plot(fpr_basic,tpr_basic,label="Age+Sex+PC: AUC="+str(auc_basic))
+plt.plot(fpr_res,tpr_res,label="+PRS: AUC="+str(auc_res))
+plt.plot(fpr_full,tpr_full,label="+AbExp: AUC="+str(auc_full))
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.legend(loc=4)
+plt.show()
+
+# %%
+fig, ax = plt.subplots()
+sklearn.metrics.PrecisionRecallDisplay.from_predictions(y_test, basic_pred_prob_test, name="Age+Sex+PC:" ,ax=ax)
+sklearn.metrics.PrecisionRecallDisplay.from_predictions(y_test, restricted_pred_prob_test, name="+PRS:", ax=ax)
+sklearn.metrics.PrecisionRecallDisplay.from_predictions(y_test, full_pred_prob_test, name="+AbExpAllTissuesForSignificantGenes:",ax=ax)
+#sklearn.metrics.PrecisionRecallDisplay.from_predictions(y_test, full_b_pred_prob_test, ax=ax)
+plt.title(f"Precision-Recall curves of Logistic Regression models predicting {phenotype_col} ({phenocode})")
+plt.xlabel("Recall")
+plt.ylabel("precision")
+plt.legend(loc=1)
+plt.show()
 
 # %% [markdown]
 # # save model + predictions
@@ -678,6 +913,56 @@ plot = (
     )
 )
 display(plot)
+
+# %%
+plot_df = pred_df[["full_model_pred", "restricted_model_pred"]].rename(columns = {"full_model_pred": "+AbExp (prediction)", "restricted_model_pred": "PRS (prediction)"})
+
+plot = (
+    pn.ggplot(plot_df, pn.aes(y="+AbExp (prediction)", x="PRS (prediction)"))
+    + pn.ggtitle(f"Predictions for {phenotype_col} of models on PRS vs. PRS + AbExp")
+    + pn.geom_bin_2d(bins=100)
+    + pn.geom_smooth(method="lm", color="red")
+    #+ pn.facet_grid("phenotype_col ~ variable")
+    + pn.scale_fill_continuous(trans = "log10")
+    #+ pn.coord_equal()
+    #+ pn.labs(
+    #    x="prediction",
+    #)
+)
+display(plot)
+
+# %%
+plot_df = test_df[[c for c in test_df.columns if c in ["individual"]+abexp_variables]]
+plot_df = pd.melt(plot_df, id_vars=["individual"], value_vars=list(set(abexp_variables).intersection(set(plot_df.columns)))).rename(columns={"value": "score"})
+plot_df[["gene_id", "score_type"]] = plot_df["variable"].str.split("@",expand=True)
+plot_df["gene_symbol"] = plot_df["gene_id"]
+plot_df = pd.merge(plot_df, pred_df[["individual", "residuals_restricted"]])
+
+# %%
+plot = (
+    pn.ggplot(plot_df, pn.aes(x="score", y="residuals_restricted"))
+    + pn.stat_bin2d(bins=100)
+    + pn.geom_smooth(method = "lm", color="red")#, se = FALSE)
+    + pn.facet_grid("gene_symbol ~ score_type", scales="free_x")
+#     + pn.labs(
+#         title=f"{phenotype_col}"
+#     )
+    + pn.theme(aspect_ratio=1/2, figure_size = (80, 160))
+)
+plot
+
+# %%
+plot_max_df = plot_df.loc[plot_df.groupby(["individual", "gene_id"])['score'].idxmax()].rename(columns={"residuals_restricted": "residual"})
+plot_max_df["score_type"] = "AbExp_max"
+plot = (
+    pn.ggplot(plot_max_df, pn.aes(x="score", y="residual"))
+    + pn.stat_bin2d(bins=100)
+    + pn.geom_smooth(method = "lm", color="red")#, se = FALSE)
+    + pn.facet_grid("gene_symbol ~ score_type", scales="free_x")
+    + pn.theme(aspect_ratio=1/2)
+    #+ pn.ggtitle(f"Residuals of common PRS and {phenotype_col} vs. AbExpMax")
+)
+plot
 
 # %%
 snakemake.params["output_basedir"]
